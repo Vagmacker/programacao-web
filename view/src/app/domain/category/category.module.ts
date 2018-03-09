cec556f4d0b8971041c0edd245d5cb6c788d91b6
Joao Vagmacker <vagmackerdsm@gmail.com>
2018-03-09 19:15:51 -0300

    resolvendo o problema do HttpClientModule na view

--- view/src/app/domain/category/category.module.ts
+++ view/src/app/domain/category/category.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { CategoryService } from './category.service';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategorySaveComponent } from './category-save/category-save.component';
@@ -11,6 +12,7 @@ import { CategoryRoutingModule } from './category-routing.module';
     CommonModule,
     RouterModule,
     CategoryRoutingModule,
+    HttpClientModule
   ],
   declarations: [
     CategorySaveComponent,
